07da947cab8894d69099f71d33fac8b38c5fe4e6
Dj Meechlouch <Meechlouch@gmail.com>
2021-02-03 22:56:36 -0600

    html routes are firing

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,11 +5,11 @@ module.exports = function (app) {
     res.sendFile(path.join("../public/index.html"));
   });
 
-  //   app.get("/stats", (req, res) => {
-  //     res.sendFile(path.join("../public/stats.html"));
-  //   });
+  app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
 
-  //   app.get("/exercise", (req, res) => {
-  //     res.sendFile(path.join("../public/exercise.html"));
-  //   });
+  app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
 };
