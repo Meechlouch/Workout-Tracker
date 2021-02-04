8903620356b7823a98445253e6bd5ce8a1900f6b
Dj Meechlouch <Meechlouch@gmail.com>
2021-02-03 21:34:31 -0600

    server file added

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
+const workout = require("./models");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+const db = require("./models");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false,
+  useFindAndModify: true,
+});
+
+require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+
+app.listen(PORT, () => {
+  console.log(`App listening on http://localhost:${PORT}`);
 });
