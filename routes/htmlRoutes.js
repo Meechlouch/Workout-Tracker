edbdae7502216d4f64e627d88f9b8390ac8554c0
Dj Meechlouch <Meechlouch@gmail.com>
2021-02-03 22:05:04 -0600

    file updated

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,4 +4,12 @@ module.exports = function (app) {
   app.get("/", (req, res) => {
     res.sendFile(path.join("../public/index.html"));
   });
+
+  //   app.get("/stats", (req, res) => {
+  //     res.sendFile(path.join("../public/stats.html"));
+  //   });
+
+  //   app.get("/exercise", (req, res) => {
+  //     res.sendFile(path.join("../public/exercise.html"));
+  //   });
 };
