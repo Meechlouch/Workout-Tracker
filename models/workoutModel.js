83ef85c34532cad272e098505fb4aaffda5080f0
Dj Meechlouch <Meechlouch@gmail.com>
2021-02-05 23:50:45 -0600

    routes working with schema update

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -2,34 +2,40 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  type: {
-    type: String,
-    unique: true,
-    required: true,
+  day: {
+    type: Date,
+    default: () => new Date(),
   },
-
-  name: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-
-  duration: {
-    type: Number,
-  },
-
-  weight: {
-    type: Number,
-    required: true,
-  },
-
-  reps: {
-    type: Number,
-  },
-
-  sets: {
-    type: Number,
-  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Enter an exercise type",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter an exercise name",
+      },
+      duration: {
+        type: Number,
+        required: "Enter an exercise duration in minutes",
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
